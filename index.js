9d37bcfc1048aef159f5c88501ab8cac34159ef9
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-25 00:21:49 +0500

    arthematic operator's

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+// Arthmetic Operators (+, -, *, /, %, **)
+// Addition 
+// let num1: number = 10;
+// let num2: number = 4;
+// let addResult: Number = num1 + num2;
+// console.log(addResult);
+// subtraction
+// let num1: number = 30;
+// let num2: number = 17;
+// let subResult: Number = num1 - num2;
+// console.log(subResult);
+// Multiplication 
+// let num1: number = 7;
+// let num2: number = 8;
+// let multiplyResult: Number = num1 * num2;
+// console.log(multiplyResult);
+// Division 
+// let num1: number = 8;
+// let num2: number = 4;
+// let divResult: Number = num1 / num2;
+// console.log(divResult);
+// Modulus
+// let num1: number = 10;
+// let num2: number = 2;
+// let modulusResult: Number = num1 % num2;
+// console.log(modulusResult);
+// Exponentation
+let num1 = 2;
+let num2 = 3;
+let expoResult = num1 ** num2;
+console.log(expoResult);
